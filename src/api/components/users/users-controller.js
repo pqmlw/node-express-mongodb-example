b8c8d2be5da2467d0aa221758f95f9a1753d799d
pqmlw <paquita.535230062@stu.untar.ac.id>
2024-04-05 22:08:25 +0700

    fungsi untuk prevent email dupe

--- src/api/components/users/users-controller.js
+++ src/api/components/users/users-controller.js
@@ -47,16 +47,19 @@ async function getUser(request, response, next) {
  */
 async function createUser(request, response, next) {
   try {
-    const name = request.body.name;
-    const email = request.body.email;
-    const password = request.body.password;
+    const { name, email, password } = request.body;
 
+    // Check if the email is already taken
+    const isTaken = await usersService.isEmailTaken(email);
+    
+    if (isTaken) {
+      throw errorResponder(errorTypes.EMAIL_ALREADY_TAKEN, 'Email is already taken');
+    }
+
+    // Proceed with user creation logic if email is not taken
     const success = await usersService.createUser(name, email, password);
     if (!success) {
-      throw errorResponder(
-        errorTypes.UNPROCESSABLE_ENTITY,
-        'Failed to create user'
-      );
+      throw errorResponder(errorTypes.UNPROCESSABLE_ENTITY, 'Failed to create user');
     }
 
     return response.status(200).json({ name, email });
@@ -75,15 +78,19 @@ async function createUser(request, response, next) {
 async function updateUser(request, response, next) {
   try {
     const id = request.params.id;
-    const name = request.body.name;
-    const email = request.body.email;
+    const { name, email } = request.body;
 
+    // Check if the new email is already taken by another user (excluding the current user)
+    const isTaken = await usersService.isEmailTakenByOtherUsers(id, email);
+    // prevent dupe emails 
+    if (isTaken) {
+      throw errorResponder(errorTypes.EMAIL_ALREADY_TAKEN, 'Email is already taken');
+    }
+
+    // Proceed with user update logic if email is not taken
     const success = await usersService.updateUser(id, name, email);
     if (!success) {
-      throw errorResponder(
-        errorTypes.UNPROCESSABLE_ENTITY,
-        'Failed to update user'
-      );
+      throw errorResponder(errorTypes.UNPROCESSABLE_ENTITY, 'Failed to update user');
     }
 
     return response.status(200).json({ id });
